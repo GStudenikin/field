1639c97a1fe0608c5df70873128030a6d7fda642
GStudenikin <bogart250595@gmail.com>
2025-07-29 12:54:11 +0700

    Finished work on General tab class

--- pages/pipe_editor/tabs/general.py
+++ pages/pipe_editor/tabs/general.py
@@ -22,17 +22,17 @@ class General(BasePage):
         "Тип основания": (AppiumBy.XPATH, "//android.widget.TextView[@text='Тип основания']"),
         "Тип фундамента": (AppiumBy.XPATH, "//android.widget.TextView[@text='Тип фундамента']"),
         "Материал фундамента": (AppiumBy.XPATH, "//android.widget.TextView[@text='Материал фундамента']"),
-        "Высота насыпи": (AppiumBy.XPATH, "//android.widget.TextView[@text='Высота насыпи, м']"),
-        "Глубина заложения": (AppiumBy.XPATH, "//android.widget.TextView[@text='Глубина заложения, м']"),
-        "Толщина засыпки": (AppiumBy.XPATH, "//android.widget.TextView[@text='Толщина засыпки, м']"),
+        "Высота насыпи": (AppiumBy.XPATH, "//android.widget.EditText[.//android.widget.TextView[@text='Высота насыпи, м']]"),
+        "Глубина заложения": (AppiumBy.XPATH, "//android.widget.EditText[.//android.widget.TextView[@text='Глубина заложения, м']]"),
+        "Толщина засыпки": (AppiumBy.XPATH, "//android.widget.EditText[.//android.widget.TextView[@text='Толщина засыпки, м']]"),
         "Помеха": (AppiumBy.XPATH, "//android.widget.TextView[@text='Помеха']"),
-        "Угол пересечения": (AppiumBy.XPATH, "//android.widget.TextView[@text='Угол пересечения, º']"),
-        "Наименование водотока": (AppiumBy.XPATH, "//android.widget.TextView[@text='Наименование водотока']"),
+        "Угол пересечения": (AppiumBy.XPATH, "//android.widget.EditText[.//android.widget.TextView[@text='Угол пересечения, º']]"),
+        "Наименование водотока": (AppiumBy.XPATH, "//android.widget.EditText[.//android.widget.TextView[@text='Наименование водотока']]"),
         "Тип трубопровода": (AppiumBy.XPATH, "//android.widget.TextView[@text='Тип трубопровода']"),
-        "Осевая нагрузка": (AppiumBy.XPATH, "//android.widget.TextView[@text='Осевая нагрузка, т']"),
-        "Год постройки": (AppiumBy.XPATH, "//android.widget.TextView[@text='Год постройки (реконструкции)']"),
-        "Год капремонта": (AppiumBy.XPATH, "//android.widget.TextView[@text='Год капремонта']"),
-        "Комментарий": (AppiumBy.XPATH, "//android.widget.TextView[@text='Комментарий']"),
+        "Осевая нагрузка": (AppiumBy.XPATH, "//android.widget.EditText[.//android.widget.TextView[@text='Осевая нагрузка, т']]"),
+        "Год постройки": (AppiumBy.XPATH, "//android.widget.EditText[.//android.widget.TextView[@text='Год постройки (реконструкции)']]"),
+        "Год капремонта": (AppiumBy.XPATH, "//android.widget.EditText[.//android.widget.TextView[@text='Год капремонта']]"),
+        "Комментарий": (AppiumBy.XPATH, "//android.widget.EditText[.//android.widget.TextView[@text='Комментарий']]"),
         #Отсутствует идентификатор
         "Запись звука": "ПРОПУСК"
     }
@@ -64,7 +64,7 @@ class General(BasePage):
     
     def set_position(self, position):
         self.click_element("Положение")
-        self.click((AppiumBy.XPATH, f"//android.widget.TextView[@text='{position}']"))
+        self.set_dropdown(position)
     
     def set_pipe_length(self, length):
         self.click_element("Длина трубы")
@@ -77,17 +77,85 @@ class General(BasePage):
     def set_bottom_reinforcement(self, reinforcement):
         self.swipe_until_find(self.ELEMENTS["Укреп. дна"], 0, 200)
         self.click_element("Укреп. дна")
-        self.swipe_until_find((AppiumBy.XPATH, f"//android.widget.TextView[@text='{reinforcement}']"), 0, 100, (AppiumBy.XPATH, "//android.widget.ScrollView"))
-        self.click((AppiumBy.XPATH, f"//android.widget.TextView[@text='{reinforcement}']"))
+        self.set_dropdown(reinforcement)
     
     def set_pipe_encasemant(self, encasement):
         self.swipe_until_find(self.ELEMENTS["Изоляция"], 0, 200)
         self.click_element("Изоляция")
-        self.swipe_until_find((AppiumBy.XPATH, f"//android.widget.TextView[@text='{encasement}']"), 0, 100, (AppiumBy.XPATH, "//android.widget.ScrollView"))
-        self.click((AppiumBy.XPATH, f"//android.widget.TextView[@text='{encasement}']"))
+        self.set_dropdown(encasement)
     
     def set_subgrade_soil(self, soil):
         self.swipe_until_find(self.ELEMENTS["Грунт основания"], 0, 200)
         self.click_element("Грунт основания")
-        self.swipe_until_find((AppiumBy.XPATH, f"//android.widget.TextView[@text='{soil}']"), 0, 100, (AppiumBy.XPATH, "//android.widget.ScrollView"))
-        self.click((AppiumBy.XPATH, f"//android.widget.TextView[@text='{soil}']"))
+        self.set_dropdown(soil)
+
+    def set_base_type(self, type):
+        self.swipe_until_find(self.ELEMENTS["Тип основания"], 0, 200)
+        self.click_element("Тип основания")
+        self.set_dropdown(type)
+    
+    def set_foundation_type(self, type):
+        self.swipe_until_find(self.ELEMENTS["Тип фундамента"], 0, 200)
+        self.click_element("Тип фундамента")
+        self.set_dropdown(type)
+    
+    def set_foundation_material(self, material):
+        self.swipe_until_find(self.ELEMENTS["Материал фундамента"], 0, 200)
+        self.click_element("Материал фундамента")
+        self.set_dropdown(material)
+    ###
+    def set_embankment_height(self, height):
+        self.swipe_until_find(self.ELEMENTS["Высота насыпи"], 0, 200)
+        self.click_element("Высота насыпи")
+        self.set_text(self.ELEMENTS["Высота насыпи"], str(height))
+    
+    def set_embedment_depth(self, depth):
+        self.swipe_until_find(self.ELEMENTS["Глубина заложения"], 0, 200)
+        self.click_element("Глубина заложения")
+        self.set_text(self.ELEMENTS["Глубина заложения"], str(depth))
+    
+    def set_backfill_thickness(self, value):
+        self.swipe_until_find(self.ELEMENTS["Толщина засыпки"], 0, 200)
+        self.click_element("Толщина засыпки")
+        self.set_text(self.ELEMENTS["Толщина засыпки"], str(value))
+    
+    def set_obstruction(self, obstruction):
+        self.swipe_until_find(self.ELEMENTS["Помеха"], 0, 200)
+        self.click_element("Помеха")
+        self.set_dropdown(obstruction)
+    
+    def set_intersection_angle(self, angle):
+        self.swipe_until_find(self.ELEMENTS["Угол пересечения"], 0, 200)
+        self.clear_text_field(self.ELEMENTS["Угол пересечения"])
+        self.click_element("Угол пересечения")
+        self.set_text(self.ELEMENTS["Угол пересечения"], str(angle))
+    
+    def set_watercourse_name(self, name):
+        self.swipe_until_find(self.ELEMENTS["Наименование водотока"], 0, 200)
+        self.click_element("Наименование водотока")
+        self.set_text(self.ELEMENTS["Наименование водотока"], name)
+    
+    def set_pipeline_type(self, type):
+        self.swipe_until_find(self.ELEMENTS["Тип трубопровода"], 0, 200)
+        self.click_element("Тип трубопровода")
+        self.set_dropdown(type)
+    
+    def set_axial_load(self, load):
+        self.swipe_until_find(self.ELEMENTS["Осевая нагрузка"], 0, 200)
+        self.click_element("Осевая нагрузка")
+        self.set_text(self.ELEMENTS["Осевая нагрузка"], str(load))
+    
+    def set_construction_year(self, year):
+        self.swipe_until_find(self.ELEMENTS["Год постройки"], 0, 200)
+        self.click_element("Год постройки")
+        self.set_text(self.ELEMENTS["Год постройки"], str(year))
+    
+    def set_capital_repair_year(self, year):
+        self.swipe_until_find(self.ELEMENTS["Год капремонта"], 0, 200)
+        self.click_element("Год капремонта")
+        self.set_text(self.ELEMENTS["Год капремонта"], str(year))
+    
+    def set_comment(self, comment):
+        self.swipe_until_find(self.ELEMENTS["Комментарий"], 0, 200)
+        self.click_element("Комментарий")
+        self.set_text(self.ELEMENTS["Комментарий"], comment)
