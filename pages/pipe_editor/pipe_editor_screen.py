7bc80802692a7e091bb62d371a69acc7d5e88987
GStudenikin <bogart250595@gmail.com>
2025-07-29 16:54:38 +0700

    Add set_tab method in PipeEditor

--- pages/pipe_editor/pipe_editor_screen.py
+++ pages/pipe_editor/pipe_editor_screen.py
@@ -19,3 +19,18 @@ class PipeEditorScreen(BasePage):
     
     def click_element(self, element):
         self.click(self.ELEMENTS[element])
+
+    def set_tab(self, tab_name):
+        """
+        Переключает на вкладку с заданным именем.
+        
+        Аргументы:
+            tab_name (str): Название вкладки, на которую нужно переключиться.
+        
+        Примечание:
+            Метод не выполняет поиск вкладки.
+            Свайп выполняется только влево (по оси X).
+        """
+        target_locator = (AppiumBy.XPATH, f"//android.widget.TextView[@text='{tab_name}']/following-sibling::android.view.View")
+        self.swipe_until_find(target_locator, -350, 0, (AppiumBy.XPATH, "//android.view.View[@content-desc='Панель выбора вкладки']"))
+        self.click(target_locator)
