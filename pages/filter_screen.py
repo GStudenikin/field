499d63940e89116f3b118445fe9bd33bec47c360
GStudenikin <bogart250595@gmail.com>
2025-08-01 16:31:19 +0700

    Add docstrings in FilterScreen and PipesScreen

--- pages/filter_screen.py
+++ pages/filter_screen.py
@@ -20,23 +20,54 @@ class FilterScreen(BasePage):
         super().__init__(driver)
     
     def click_element(self, element):
+        """
+        Выполняет нажатие на элемент по ключу из словаря ELEMENTS.
+
+        Аргументы:
+            element (str): Ключ элемента в словаре ELEMENTS.
+        """
         self.click(self.ELEMENTS[element])
     
     def set_start(self, start):
+        """
+        Устанавливает значение в поле "Начало".
+
+        Аргументы:
+            start (int): значение, которое будет установлено.
+        """
         self.click_element("Начало")
-        self.set_text(self.ELEMENTS["Начало"], start)
+        self.set_text(self.ELEMENTS["Начало"], str(start))
 
     def set_end(self, end):
+        """
+        Устанавливает значение в поле "Конец".
+
+        Аргументы:
+            end (int): значение, которое будет установлено.
+        """
         self.click_element("Конец")
-        self.set_text(self.ELEMENTS["Конец"], end)
+        self.set_text(self.ELEMENTS["Конец"], str(end))
 
     def set_date(self, field, date):
+        """
+        Открывает поле даты и устанавливает указанную дату через компонент DatePicker.
+
+        Аргументы:
+            field (str): Ключ из словаря ELEMENTS для поля даты.
+            date (str): Дата в формате 'ДД.ММ.ГГГГ'.
+        """
         self.click_element(field)
         date_picker = DatePicker(self.driver)
         date_picker.set_date(date)
     
     def clear(self):
+        """
+        Очищает фильтр нажатием на кнопку 'ОЧИСТИТЬ'
+        """
         self.click_element("ОЧИСТИТЬ")
     
     def accept(self):
+        """
+        Применяет фильтр нажатием на кнопку 'ПРИМЕНИТЬ'
+        """
         self.click_element("ПРИМЕНИТЬ")
--- pages/pipes_screen.py
+++ pages/pipes_screen.py
@@ -20,8 +20,20 @@ class PipesScreen(BasePage):
         super().__init__(driver)
     
     def click_element(self, element):
+        """
+        Выполняет нажатие на элемент по ключу из словаря ELEMENTS.
+
+        Аргументы:
+            element (str): Ключ элемента в словаре ELEMENTS.
+        """
         self.click(self.ELEMENTS[element])
     
     def set_sorting(self,sorting_type):
+        """
+        Устанавливает тип сортировки из выпадающего списка.
+
+        Аргументы:
+            sorting_type (str): Текстовое значение сортировки, которое нужно выбрать.
+        """
         self.click_element("Сортировка")
         self.click((AppiumBy.XPATH, f"//android.widget.TextView[@text='{sorting_type}']"))
